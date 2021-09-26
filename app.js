ccb1103d05181bfdf8aa6a5d87968c9e7a45153a
maiyehezkel <maiyehezkel105@gmail.com>
2021-09-26 23:03:25 +0300

    contact mail

--- app.js
+++ app.js
@@ -1,13 +1,71 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const sendMail = require('./contactMail');
 
-var app = express();
+const app = express();
 
+// handlebars
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+// body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Routes
 app.get('/', function (req, res) {
     res.render('home');
 });
+
+app.get('/contect-us', function (req, res) {
+    res.render('contectUs');
+});
+//form-post
+app.post('/sent', (req,res)=>{
+    const { name, number, email, message, subject } = req.body;
+    console.log(req.body);
+    sendMail(number , name, subject, email,message, function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log('EMAIL SENT!');
+        }
+    });
+});
+
+app.get('/catalog', function (req, res) {
+    res.render('catalog');
+});
+
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+// 
+app.get('/catalog/salads', function (req, res) {
+    res.render('salads');
+});
+
+app.get('/catalog/cheeses', function (req, res) {
+    res.render('cheeses');
+});
+
+app.get('/catalog/sauces', function (req, res) {
+    res.render('sauces');
+});
+
+app.get('/catalog/fish', function (req, res) {
+    res.render('fish');
+});
+
+app.get('/catalog/vegetables', function (req, res) {
+    res.render('vegetables');
+});
+
+app.get('/catalog/meats', function (req, res) {
+    res.render('meats');
+});
+
 
 app.listen(3000);
---
+++ contactMail.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'almacustomerserviceil@gmail.com',
+    pass: '025151531Fy' 
+  }
+});
+
+const sendMail = (number, name, subject, email,message)=>{
+    const mailOptions = {
+        from: '"ALMA WEBSITE" <almacustomerserviceil@gmail.com>',
+        to: 'almacustomerserviceil@gmail.com',
+        name: name,
+        number: number,
+        subject: subject,
+        text: "שם:"+name+" טלפון:"+number+" כתובת מייל:"+email+" הודעה:"+message
+   
+    };
+    transporter.sendMail(mailOptions, function(err, info){
+        if (err) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+        });
+    }
+    module.exports = sendMail;
+
+
+
+
+
+
+
